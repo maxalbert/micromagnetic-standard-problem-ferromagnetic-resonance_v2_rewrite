86d5cdef137e46d989b7af238f650f24218dee4c
Maximilian Albert <maximilian.albert@gmail.com>
2016-01-18 17:49:08 +0000

    Add proper test for get_spectrum_via_method_1().

--- src/postprocessing/fft_handler.py
+++ src/postprocessing/fft_handler.py
@@ -12,11 +12,21 @@ class FFTHandler(object):
     functions, but it provides a unified interface which also uses
     the naming conventions in the paper ("method 1", "method 2").
     """
-    def get_FFT_coeffs_of_average_m(self, m_avg):
+
+    def _get_FFT_coeffs_of_average_m(self, m_avg):
         fft_coeffs = np.fft.rfft(m_avg, axis=0)
         return fft_coeffs
 
     def get_fft_frequencies(self, timesteps, unit='Hz'):
+        """
+        Return the FFT sample frequencies for the given timesteps.
+
+        The argument `unit` determines the unit of the returned
+        frequencies. It can be either 'Hz' or 'GHz'.
+
+        Note: The timesteps passed into this function must be given
+              in units of second (not nanosecond)!
+        """
         timestep_unit = util.get_timestep_unit(unit)
         dt = util.convert_to_unit(timesteps[1] - timesteps[0], timestep_unit)
 
@@ -45,7 +55,7 @@ class FFTHandler(object):
             Power spectral densities of the Fourier-transformed magnetisation data.
 
         """
-        fft_m_avg = self.get_FFT_coeffs_of_average_m(m_avg)
+        fft_m_avg = self._get_FFT_coeffs_of_average_m(m_avg)
         psd_m_avg = np.abs(fft_m_avg)**2
         # FIXME: We ignore the last element for now so that we can
         #        compare with the existing reference data.
--- tests/unit_tests/test_fft_handler.py
+++ tests/unit_tests/test_fft_handler.py
@@ -1,3 +1,4 @@
+from __future__ import division
 import numpy as np
 import os
 import unittest
@@ -17,6 +18,7 @@ class TestFFTHandler(unittest.TestCase):
         """
         FFTHandler.get_fft_frequencies() returns expected frequencies of power spectrum.
         """
+
         timesteps = np.linspace(5e-12, 20e-9, 4000)
         fft_freqs_expected = np.linspace(0, 100e9, 2000, endpoint=False)
         fft_freqs_GHz_expected = np.linspace(0, 100, 2000, endpoint=False)
@@ -29,16 +31,27 @@ class TestFFTHandler(unittest.TestCase):
 
     def test__get_spectrum_via_method_1_returns_array_of_expected_shape(self):
         """
-        FFTHandler.get_spectrum_via_method_1() returns array of expected shape.
+        FFTHandler.get_spectrum_via_method_1() returns expected result for a known signal.
         """
+
         fft_handler = FFTHandler()
 
-        timesteps = np.linspace(5e-12, 20e-9, 4000)
-        m_avg_y = np.cos(timesteps)
+        num_timesteps = 4000
+        num_oscillations = 3
+        TOL = 1e-14
 
-        for component in ('x', 'y', 'z'):
-            spectrum = self.fft_handler.get_spectrum_via_method_1(m_avg_y)
-            self.assertEquals(spectrum.shape, (2000,))
+        # Construct an artificial magnetisation precession with known FFT transform.
+        # This precession is a pure cosine wave with a few oscillations.
+        timesteps = np.linspace(5e-12, 20e-9, num_timesteps)
+        amplitude = 1. / (num_timesteps // 2)
+        m_avg_y = amplitude * np.cos(num_oscillations * (2*np.pi) * timesteps / max(timesteps))
 
-            import warnings
-            warnings.warn("TODO: Make sure the computed result is as expected!")
+        # Compute the FFT transform and check that the resulting FFT coefficients are as expected:
+        #
+        # (i) The single FFT coefficient corresponding to the number of precessions should be equal to one.
+        spectrum = self.fft_handler.get_spectrum_via_method_1(m_avg_y)
+        self.assertTrue(np.isclose(spectrum[num_oscillations], 1.0, atol=TOL, rtol=0))
+
+        # (ii) All other FFT coefficients should be zero.
+        spectrum[num_oscillations] = 0
+        self.assertTrue(np.allclose(spectrum, 0, atol=TOL, rtol=0))
