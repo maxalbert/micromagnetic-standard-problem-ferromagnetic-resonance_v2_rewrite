6124e36b190a1b905804b7b5c86f85bb6b7e1a17
Maximilian Albert <maximilian.albert@gmail.com>
2016-01-20 15:48:40 +0000

    Add test for Figure 2 generated from Nmag data.

--- tests/integration_tests/test_reproduce_figures_from_reference_data.py
+++ tests/integration_tests/test_reproduce_figures_from_reference_data.py
@@ -6,12 +6,21 @@ from postprocessing import DataReader, FigurePlotter
 TOL = 0
 
 here = Path(__file__).parent.resolve()
-REF_DATA_DIR = here.joinpath('../../micromagnetic_simulation_data/reference_data/oommf')
+REF_DATA_DIR_OOMMF = here.joinpath('../../micromagnetic_simulation_data/reference_data/oommf/')
+REF_DATA_DIR_NMAG = here.joinpath('../../micromagnetic_simulation_data/reference_data/nmag/')
 
 
 @image_comparison(baseline_images=['figure_2_OOMMF'], extensions=['png', 'pdf'], tol=TOL)
-def test__make_figure_2():
-    data_reader = DataReader(REF_DATA_DIR, data_format='OOMMF')
+def test__make_figure_2_OOMMF():
+    data_reader = DataReader(REF_DATA_DIR_OOMMF, data_format='OOMMF')
+    figure_plotter = FigurePlotter(data_reader)
+
+    fig = figure_plotter.make_figure_2()
+
+
+@image_comparison(baseline_images=['figure_2_Nmag'], extensions=['png', 'pdf'], tol=TOL)
+def test__make_figure_2_Nmag():
+    data_reader = DataReader(REF_DATA_DIR_NMAG, data_format='Nmag')
     figure_plotter = FigurePlotter(data_reader)
 
     fig = figure_plotter.make_figure_2()
